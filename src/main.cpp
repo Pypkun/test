bcf812cf29bbf750132a382ab8dc8fffab0a6df9
Pypkun <andrejtelih@gmail.com>
2024-04-08 13:47:33 +0700

    bug fixing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iosteram>
+using namespace std;
 
 int main() {
-	std::cout << "hI GIT_TEST" << std::endl;
+	cout << "hI GIT_TEST" << endl;
+	
+	int a, b, res;
+	char ch;
+	cout << "введите первое число: ";
+	cin >> a;
+	cout << "вветиде второе число: ";
+	cin >> b;
+	cin >> ch;
+	cout << "введите тип арифмического действия: ";
+	switch (ch)
+	{
+	case '+': res = a + b; break;
+	case '-': res = a - b; break;
+	case '*': res = a * b; break;
+	case '/': res = a / b; break;
+	default:
+		cout << "я вас не понимаю"; break;
+	}
+	
+	
+
+
+	
 	return 0;
 }
